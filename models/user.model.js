b84e957505bfa3e03b7536beec4268fbf6ae1a62
Agustin Guerrero <agustinguerrero.reyes@gmail.com>
2022-08-12 14:12:43 +0200

    commit inicial

--- models/user.model.js
+++ models/user.model.js
@@ -13,6 +13,6 @@ UserSchema.method('toJSON', function () {
   const { __v, _id, ...object } = this.toObject();
   object.uid = _id;
   return object;
-})
+}) 
 
 module.exports = model("User", UserSchema);
